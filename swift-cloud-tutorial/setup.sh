55a39486622a4231a98f308496801ad5547ad42e
Yadu Nand Babuji <yadudoc1729@gmail.com>
2014-05-02 11:21:09 -0500

    Committing tests x

---
+++ swift-cloud-tutorial/setup.sh
@@ -0,0 +1,26 @@
+# ensure that this script is being sourced
+if [ ${BASH_VERSINFO[0]} -gt 2 -a "${BASH_SOURCE[0]}" = "${0}" ] ; then
+  echo ERROR: script ${BASH_SOURCE[0]} must be executed as: source ${BASH_SOURCE[0]}
+  exit 1
+fi
+
+echo Swift version is $(swift -version)
+rm -f swift.log
+
+# Setting scripts folder to the PATH env var.
+TUTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ _$(which cleanup 2>/dev/null) != _$TUTDIR/bin/cleanup ]; then
+  echo Adding $TUTDIR/bin:$TUTDIR/app: to front of PATH
+  PATH=$TUTDIR/bin:$TUTDIR/app:$PATH
+else
+  echo Assuming $TUTDIR/bin:$TUTDIR/app: is already at front of PATH
+fi
+
+# Start coaster-service and generate sites.xml
+for p in 04 05 06; do
+  cp ../compute-engine/swift.properties part${p}/swift.properties
+done
+
+return
+
