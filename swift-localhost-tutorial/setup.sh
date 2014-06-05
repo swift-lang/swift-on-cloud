21db65b577034b130fc0b56ab64bd5f2af2f7c48
Yadu Nand Babuji <yadudoc1729@gmail.com>
2014-06-04 19:01:49 -0500

    Updated tutorial

--- swift-localhost-tutorial/setup.sh
+++ swift-localhost-tutorial/setup.sh
@@ -20,7 +20,7 @@ then
     export SWIFT=/usr/local/bin/swift-0.95/bin
     export PATH=$JAVA:$SWIFT:$PATH
 else # Running on local machine
-    for p in 01 02 03 04 05 06
+    for p in 03 04 05 06
     do
         cp swift.properties part${p}/swift.properties
     done
