efdd50c1888ecb8a861e25180da1e366472fb188
Yadu Nand Babuji <yadudoc1729@gmail.com>
2014-06-17 12:17:43 -0500

    Updates to doc.

--- swift-localhost-tutorial/doc/push.sh
+++ swift-localhost-tutorial/doc/push.sh
@@ -2,4 +2,10 @@
 
 # scp -r swift-cray-tutorial.html images *png login.ci.uchicago.edu:/ci/www/projects/swift/tutorials/cray
 
+pushd .
+cd ../../
+tar -cvzf swift-localhost-tutorial.tar.gz swift-localhost-tutorial
+mv swift-localhost-tutorial.tar.gz swift-localhost-tutorial/doc/
+popd
+
 tar zcf - --exclude-vcs *tar.gz *html *png images | ssh login.ci.uchicago.edu "cd /ci/www/projects/swift/tutorials/localhost; tar zxf -"
