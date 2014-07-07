d97fd8cbad6036e558e4f4b7a3162963800663ba
Yadu Babuji <yadunandb@login4.beagle.ci.uchicago.edu>
2014-07-07 14:21:17 -0500

    Committing tested tutorial material from beagle

--- swift-tutorial/test_all.sh
+++ swift-tutorial/test_all.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-for i in $(seq 1 1 5)
+
+
+SITES=('beagle' 'beagle-remote')
+for i in $(seq 4 1 6)
 do
     pushd .
     echo "============================TESTING part0$i==========================="
     cd part0$i
-    swift p$i.swift
-    if [[ $? == 0 ]]
-    then
-        echo "Cleaning up!"
-        cleanup
-    fi
+
+    for SITE in ${SITES[*]}
+    do
+        echo "Running on SITE : $SITE"
+        swift p$i.swift -site=$SITE
+        if [[ $? == 0 ]]
+        then
+            echo "Cleaning up!"
+            cleanup
+        fi
+    done
     echo -e "\n\n"
     popd
 done
